57cb670f8fa790d5457b3b0a45cf2544cbeb32c0
Shigetorum <Shigetorum@hotmail.com>
2021-11-29 17:20:50 +0100

    asdads

---
+++ types/main/index.d.ts
@@ -0,0 +1,30 @@
+import { Message } from "discord.js"
+
+declare module 'configuration' {
+    /**
+     * Configuration of your bot.
+     */
+	export type IConfiguration = {
+        /**
+         * Token of your bot.
+         */
+		token: string
+        /**
+         * Prefix of your bot.
+         */
+		prefix: string
+        /**
+         * Time to cool down the bot commands.
+         */
+		coolDown: number
+	}
+    /**
+     * Context at which we are logging.
+     */
+	export type IContext = '[Bot]' | '[Client]' | '[Server]'
+
+    /**
+     * Type for the commands
+     */
+    export type Icommand = (message: Message, arguments: string[]) => void | any 
+}
