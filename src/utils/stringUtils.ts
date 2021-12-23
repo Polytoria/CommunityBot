117669d9f7164e0ae569434666dff61d756bb28f
Alyx <25337749+Alyxsqrd@users.noreply.github.com>
2021-12-23 09:39:10 -0500

    Cleanup string utils

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -1,53 +1,17 @@
-const url_pattern = new RegExp(
-	'^(https?:\\/\\/)?' + '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + '((\\d{1,3}\\.){3}\\d{1,3}))' + '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + '(\\?[;&a-z\\d%_.~+=-]*)?' + '(\\#[-a-z\\d_]*)?$',
-	'i'
-)
-
 export class stringUtils {
-	/**
-	 * ### Transforms number periods to comas. *Formatting*
-	 * @example 2.320 => "2,320"
-	 * @param number Input of which number to transform.
-	 * @returns Transformed number as a string.
-	 */
-	public static numberToComas(number: number): string {
-		return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-	}
-
-	/**
-	 * ### Custom String replacer running with RegExp support
-	 * @param string String to be replaced.
-	 * @param keyword Keyword to replace.
-	 * @param replace String to replace keywords with.
-	 * @returns Replaced String.
-	 */
-	public static async customReplace(string: string, keyword: string, replace: string) {
-		return string.replace(new RegExp(keyword, 'g'), replace)
-	}
-
 	/**
      * ### URL Parse
      * @param string URL that want to be parsed
-     * @returns 
-     * {
-     *  URLData: string,
-        searchParams: URLSearchParams,
-        spitted: Array<string>
-     * }
-    */
+     * @returns {urlData: string, searchParams: URLSearchParams, splitted: string[]}
+     */
+	public static urlParse(url: string): {urlData: URL; searchParams: URLSearchParams; splitted: string[]} {
+		const urlConstruct = new URL(url)
 
-	public static urlParse(url: string) {
-		let urlConstruct = new URL(url)
-		let searchParams_url = urlConstruct.searchParams
-		let spitted_url: Array<string> = url.split('/')
-
-		let returnResult: {URLData: URL; searchParams: URLSearchParams; spitted: Array<string>} = {
-			URLData: urlConstruct,
-			searchParams: searchParams_url,
-			spitted: spitted_url
+		return {
+			urlData: urlConstruct,
+			searchParams: urlConstruct.searchParams,
+			splitted: url.split('/')
 		}
-
-		return returnResult
 	}
 
 	/**
@@ -55,27 +19,18 @@ export class stringUtils {
 	 * @param string URL that want to be parsed
 	 * @returns array Numbers that has been found in URL
 	 */
-	public static getNumbersFromURL(string: string) {
-		let result = stringUtils.urlParse(string)
-		let splitted = result.spitted
+	public static getNumbersFromURL(string: string): number[] {
+		const parsed = stringUtils.urlParse(string)
+		const result: number[] = []
 
-		let returnresult: Array<string> = []
+		parsed.splitted.forEach(function (item: string) {
+			const value = parseInt(item)
 
-		splitted.forEach(function (item: string) {
-			if (isNaN(parseInt(item)) == false) {
-				returnresult.push(item)
+			if (isNaN(value) === false) {
+				result.push(value)
 			}
 		})
 
-		return returnresult
-	}
-
-	/**
-	 * ### Is URL
-	 * @param string string that you want to checks that's url or not
-	 * @returns bool returns if it were url or not
-	 */
-	public static strIsURL(string: string) {
-		return !!url_pattern.test(string)
+		return result
 	}
 }
