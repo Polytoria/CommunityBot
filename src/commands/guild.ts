e2aef19f5ea6693b2864aeaee4aaa964ffa12009
Shiggy <notshigetora@gmail.com>
2022-02-07 15:21:29 +0100

    [wip][fix] fix some of esLint issues & add paths

--- src/commands/guild.ts
+++ src/commands/guild.ts
@@ -1,61 +1,61 @@
-import {Message, MessageEmbed} from 'discord.js'
+import { Message, MessageEmbed } from 'discord.js'
 import axios from 'axios'
-import {responseHandler} from '../utils/responseHandler.js'
-import {userUtils} from '../utils/userUtils.js'
-import {dateUtils} from '../utils/dateUtils.js'
+import { responseHandler } from '../utils/responseHandler.js'
+import { userUtils } from '@utils/userUtils'
+import { dateUtils } from '@utils/dateUtils.js'
 
-export async function guild(message: Message, args: string[]) {
-	const guildID = parseInt(args[0])
+export async function guild (message: Message, args: string[]): Promise<Message<boolean>> {
+  const guildID = parseInt(args[0])
 
-	const response = await axios.get('https://api.polytoria.com/v1/guild/info', {params: {id: guildID}, validateStatus: () => true})
-	const data = response.data
+  const response = await axios.get('https://api.polytoria.com/v1/guild/info', { params: { id: guildID }, validateStatus: () => true })
+  const data = response.data
 
-	const errResult = responseHandler.checkError(response)
+  const errResult = responseHandler.checkError(response)
 
-	if (errResult.hasError === true) {
-		return message.channel.send(errResult.displayText)
-	}
+  if (errResult.hasError === true) {
+    return message.channel.send(errResult.displayText)
+  }
 
-	const userData = await userUtils.getUserData(data.CreatorID)
+  const userData = await userUtils.getUserData(data.CreatorID)
 
-	const Embed = new MessageEmbed({
-		title: data.Name,
-		description: data.Description,
-		url: 'https://polytoria.com/guilds/' + data.ID.toString(),
-		thumbnail: {
-			url: data.Thumbnail
-		},
-		color: '#ff5454',
-		fields: [
-			{
-				name: '🗂️ Creator ID 🗂️',
-				value: data.CreatorID.toString(),
-				inline: true
-			},
-			{
-				name: '👷 Creator Name 👷',
-				value: userData.Username,
-				inline: true
-			},
-			{
-				name: '🎉 Members 🎉',
-				value: data.Members.toLocaleString(),
-				inline: true
-			},
-			{
-				name: '✅ Is Verified ✅',
-				value: data.IsVerified.toString(),
-				inline: true
-			},
-			{
-				name: '🔥 Created At 🔥',
-				value: dateUtils.atomTimeToDisplayTime(data.CreatedAt),
-				inline: true
-			}
-		]
-	})
+  const Embed = new MessageEmbed({
+    title: data.Name,
+    description: data.Description,
+    url: 'https://polytoria.com/guilds/' + data.ID.toString(),
+    thumbnail: {
+      url: data.Thumbnail
+    },
+    color: '#ff5454',
+    fields: [
+      {
+        name: '🗂️ Creator ID 🗂️',
+        value: data.CreatorID.toString(),
+        inline: true
+      },
+      {
+        name: '👷 Creator Name 👷',
+        value: userData.Username,
+        inline: true
+      },
+      {
+        name: '🎉 Members 🎉',
+        value: data.Members.toLocaleString(),
+        inline: true
+      },
+      {
+        name: '✅ Is Verified ✅',
+        value: data.IsVerified.toString(),
+        inline: true
+      },
+      {
+        name: '🔥 Created At 🔥',
+        value: dateUtils.atomTimeToDisplayTime(data.CreatedAt),
+        inline: true
+      }
+    ]
+  })
 
-	return message.channel.send({
-		embeds: [Embed]
-	})
+  return message.channel.send({
+    embeds: [Embed]
+  })
 }
--- src/commands/user/friends.ts
+++ src/commands/user/friends.ts
@@ -1,108 +1,108 @@
-import {Message, MessageEmbed, MessageActionRow, MessageButton} from 'discord.js'
+import { Message, MessageEmbed, MessageActionRow, MessageButton } from 'discord.js'
 import axios from 'axios'
-import {userUtils} from '../../utils/userUtils.js'
-import {v4} from 'uuid'
+import { userUtils } from '@utils/userUtils.js'
+import { v4 } from 'uuid'
 
-export async function friends(message: Message, args: string[]) {
-	const userData = await userUtils.getUserDataFromUsername(args[0])
+export async function friends (message: Message, args: string[]) {
+  const userData = await userUtils.getUserDataFromUsername(args[0])
 
-	if (!userData.ID) {
-		return message.reply("User not found!")
-	}
+  if (!userData.ID) {
+    return message.reply('User not found!')
+  }
 
-	let currentPage = 1
+  let currentPage = 1
 
-	const apiURL = 'https://api.polytoria.com/v1/users/friends?id=' + userData.ID.toString()
+  const apiURL = 'https://api.polytoria.com/v1/users/friends?id=' + userData.ID.toString()
 
-	const response = await axios.get(apiURL, {validateStatus: () => true})
-	const data = response.data
+  const response = await axios.get(apiURL, { validateStatus: () => true })
+  const data = response.data
 
-	// Change Page Function, Fetch current page
-	async function changePage(): Promise<string> {
-		const apiURL = 'https://api.polytoria.com/v1/users/friends?id=' + userData.ID.toString() + '&page=' + currentPage
+  // Change Page Function, Fetch current page
+  async function changePage (): Promise<string> {
+    const apiURL = 'https://api.polytoria.com/v1/users/friends?id=' + userData.ID.toString() + '&page=' + currentPage
 
-		const response = await axios.get(apiURL, {validateStatus: () => true})
-		let resultString: string = ''
+    const response = await axios.get(apiURL, { validateStatus: () => true })
+    let resultString: string = ''
 
-		//@ts-expect-error
-		response.data.Friends.forEach((item) => {
-			resultString += `[${item.Username}](https://polytoria.com/user/${item.ID})\n`
-		})
+    // @ts-expect-error
+    response.data.Friends.forEach((item) => {
+      resultString += `[${item.Username}](https://polytoria.com/user/${item.ID})\n`
+    })
 
-		return resultString
-	}
+    return resultString
+  }
 
-	const embed = new MessageEmbed({
-		title: userData.Username + "'s Friends.",
-		url: `https://polytoria.com/user/${userData.ID}/friends`,
-		color: '#ff5454',
-		thumbnail: {
-			url: `https://polytoria.com/assets/thumbnails/avatars/${userData.AvatarHash}.png`
-		},
-		description: ''
-	})
+  const embed = new MessageEmbed({
+    title: userData.Username + "'s Friends.",
+    url: `https://polytoria.com/user/${userData.ID}/friends`,
+    color: '#ff5454',
+    thumbnail: {
+      url: `https://polytoria.com/assets/thumbnails/avatars/${userData.AvatarHash}.png`
+    },
+    description: ''
+  })
 
-	// Fetch Friends
-	const friendsData: string = await changePage()
-	embed.description = friendsData
+  // Fetch Friends
+  const friendsData: string = await changePage()
+  embed.description = friendsData
 
-	// Generate Button ID base on current time
-	const buttonID: string = v4()
-	const leftBtnID: string = 'left' + buttonID
-	const pageNum: string = 'page' + buttonID
-	const rightBtnID: string = 'right' + buttonID
+  // Generate Button ID base on current time
+  const buttonID: string = v4()
+  const leftBtnID: string = 'left' + buttonID
+  const pageNum: string = 'page' + buttonID
+  const rightBtnID: string = 'right' + buttonID
 
-	// Create Buttons
-	const leftBtn: MessageButton = new MessageButton().setCustomId(leftBtnID).setLabel('◀').setStyle('PRIMARY').setDisabled(true)
+  // Create Buttons
+  const leftBtn: MessageButton = new MessageButton().setCustomId(leftBtnID).setLabel('◀').setStyle('PRIMARY').setDisabled(true)
 
-	const pageNumBtn: MessageButton = new MessageButton().setCustomId(pageNum).setLabel(`Page ${currentPage.toString()} of ${data.Pages.toString()}`).setStyle('SECONDARY')
+  const pageNumBtn: MessageButton = new MessageButton().setCustomId(pageNum).setLabel(`Page ${currentPage.toString()} of ${data.Pages.toString()}`).setStyle('SECONDARY')
 
-	const rightBtn: MessageButton = new MessageButton().setCustomId(rightBtnID).setLabel('▶').setStyle('PRIMARY')
+  const rightBtn: MessageButton = new MessageButton().setCustomId(rightBtnID).setLabel('▶').setStyle('PRIMARY')
 
-	const row = new MessageActionRow().addComponents(leftBtn).addComponents(pageNumBtn).addComponents(rightBtn)
+  const row = new MessageActionRow().addComponents(leftBtn).addComponents(pageNumBtn).addComponents(rightBtn)
 
-	const filter = () => true
+  const filter = () => true
 
-	// Create Interaction event for 2 minutes
-	const collector = message.channel.createMessageComponentCollector({filter, time: 120000})
+  // Create Interaction event for 2 minutes
+  const collector = message.channel.createMessageComponentCollector({ filter, time: 120000 })
 
-	// Listen for Button Interaction
-	collector.on('collect', async (i) => {
-		if (i.user.id !== message.author.id) {
-			return;
-		}
-		// Change page
-		if (i.customId === rightBtnID) {
-			currentPage++
-		} else if (i.customId === leftBtnID) {
-			currentPage--
-		}
+  // Listen for Button Interaction
+  collector.on('collect', async (i) => {
+    if (i.user.id !== message.author.id) {
+      return
+    }
+    // Change page
+    if (i.customId === rightBtnID) {
+      currentPage++
+    } else if (i.customId === leftBtnID) {
+      currentPage--
+    }
 
-		// Update button state
-		if (currentPage >= data.Pages) {
-			rightBtn.setDisabled(true)
-			currentPage = data.Pages
-		} else {
-			rightBtn.setDisabled(false)
-		}
+    // Update button state
+    if (currentPage >= data.Pages) {
+      rightBtn.setDisabled(true)
+      currentPage = data.Pages
+    } else {
+      rightBtn.setDisabled(false)
+    }
 
-		if (currentPage <= 1) {
-			leftBtn.setDisabled(true)
-			currentPage = 1
-		} else {
-			leftBtn.setDisabled(false)
-		}
+    if (currentPage <= 1) {
+      leftBtn.setDisabled(true)
+      currentPage = 1
+    } else {
+      leftBtn.setDisabled(false)
+    }
 
-		// Set Page
-		pageNumBtn.setLabel(`Page ${currentPage.toString()} of ${data.Pages.toString()}`)
+    // Set Page
+    pageNumBtn.setLabel(`Page ${currentPage.toString()} of ${data.Pages.toString()}`)
 
-		// Fetch Friends
-		const friendsData: string = await changePage()
-		embed.description = friendsData
+    // Fetch Friends
+    const friendsData: string = await changePage()
+    embed.description = friendsData
 
-		// Update Embed and Button
-		await i.update({embeds: [embed], components: [row]})
-	})
+    // Update Embed and Button
+    await i.update({ embeds: [embed], components: [row] })
+  })
 
-	return await message.channel.send({embeds: [embed], components: [row]})
+  return await message.channel.send({ embeds: [embed], components: [row] })
 }
