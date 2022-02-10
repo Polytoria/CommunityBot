d7a3ae310fa0ccf75ec96486949995acd529edc4
StarManTheGamer <68450308+StarManTheGamer@users.noreply.github.com>
2022-02-09 21:10:58 -0600

    Updated info logo

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -3,7 +3,7 @@ import { MessageEmbed, Message } from 'discord.js'
 export async function info (message: Message) {
   const embed: MessageEmbed = new MessageEmbed()
 
-  embed.setThumbnail('https://polytoria.com/assets/thumbnails/guilds/1.png')
+  embed.setThumbnail('https://polytoria.com/assets/catalog/12659.png')
   embed.setColor('#fe5953')
   embed.setTitle('Polytoria Community Bot')
   embed.setURL('https://discord.com/api/oauth2/authorize?client_id=905979909049028649&permissions=414464724032&scope=bot')
