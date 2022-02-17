6eaa45bf9f60db40ee4d568a26f9801103332ce4
SK-Fast <thummajil25580@gmail.com>
2022-02-17 09:14:27 +0700

    Added Help command

--- src/commands/helpPages.ts
+++ src/commands/helpPages.ts
@@ -1,3 +1,56 @@
-export default {
-
-}
+export default [
+    [
+        {
+            name: "help",
+            value: "Command you're using right now.",
+            inline: false
+        },
+        {
+            name: "catalog `id`",
+            value: "View Catalog item data by Catalog item ID\n`id` - Catalog item ID",
+            inline: false
+        },
+        {
+            name: "lookup `username/id` `type(optional)`",
+            value: "Lookup user from username or ID\n`username/id` - Their username or id\n`type` - Type, `id` or `username`",
+            inline: false
+        },
+        {
+            name: "leaderboard",
+            value: "View Polytoria leaderboard",
+            inline: false
+        },
+        {
+            name: "avatar `username`",
+            value: "View user's avatar\n`username` - User's username you want to view",
+            inline: false
+        },
+    ],
+    [
+        {
+            name: "game `id`",
+            value: "View game info from game ID\n`id` - Game id",
+            inline: false
+        },
+        {
+            name: "ping",
+            value: "Checks is the bot are online.",
+            inline: false
+        },
+        {
+            name: "status",
+            value: "View Polytoria web status.",
+            inline: false
+        },
+        {
+            name: "cookie",
+            value: "🍪",
+            inline: false
+        },
+        {
+            name: "friends `username`",
+            value: "View targeted user's friends\n`username` - User's username you want to view",
+            inline: false
+        },
+    ],
+]
