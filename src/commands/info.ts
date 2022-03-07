e295c4e6fc9240dbeea09b5f3e676063523b513b
Shiggy <48862659+Shigetorum635@users.noreply.github.com>
2022-03-07 11:37:27 +0100

    feat(version): use package.json for versioning
    fix: fixes having to bump version manually and encourages more dynamic approaches.

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -1,4 +1,5 @@
 import { MessageEmbed, Message } from 'discord.js'
+import { version } from  './../../package.json';
 
 export async function info (message: Message) {
   const embed: MessageEmbed = new MessageEmbed()
@@ -7,7 +8,7 @@ export async function info (message: Message) {
   embed.setColor('#fe5953')
   embed.setTitle('Polytoria Community Bot')
   embed.setURL('https://discord.com/api/oauth2/authorize?client_id=905979909049028649&permissions=414464724032&scope=bot')
-  embed.addField('Version', 'Running version 2.0.2')
+  embed.addField('Version', `Running version ${version}`)
   embed.addField('Invite our Bot', 'Tap the title to invite our bot directly to your server!')
   embed.addField('We are open-sourced!', 'https://github.com/Polytoria/Polytoria-Community-Bot')
   embed.addField('Rewritten by', 'Shiggy, DevPixels, baggy, and more!')
