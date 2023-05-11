c805d8b70bea36d6c314022d6c5245c111de1864
StarManTheGamer <68450308+StarManTheGamer@users.noreply.github.com>
2023-05-11 17:42:52 -0500

    Part 1 of restoring lookup command

--- src/commands/user/lookup.ts
+++ src/commands/user/lookup.ts
@@ -2,27 +2,24 @@ import { Message, MessageEmbed } from 'discord.js'
 import axios from 'axios'
 import { responseHandler } from '../../utils/responseHandler.js'
 import { dateUtils } from '../../utils/dateUtils.js'
-import { stringUtils } from '../../utils/stringUtils.js'
-import emojiUtils from '../../utils/emojiUtils.js'
 
 export async function lookUp (message: Message, args: string[]) {
   let apiURL: string = ''
 
   switch (args[1]) {
     case 'user':
-      apiURL = `https://api.polytoria.com/v1/users/getbyusername?username=${args.join(' ')}`
+      apiURL = `https://api.polytoria.com/v1/users/find?username=${args.join(' ')}`
       break
     case 'id':
-      apiURL = `https://api.polytoria.com/v1/users/user?id=${args.join(' ')}`
+      apiURL = `https://api.polytoria.com/v1/users/find?id=${args.join(' ')}`
       break
     default:
-      apiURL = `https://api.polytoria.com/v1/users/getbyusername?username=${args.join(' ')}`
+      apiURL = `https://api.polytoria.com/v1/users/find?username=${args.join(' ')}`
       break
   }
 
   const response = await axios.get(apiURL, { validateStatus: () => true })
   const data = response.data
-  let badges = ' '
 
   const errResult = responseHandler.checkError(response)
 
@@ -30,66 +27,22 @@ export async function lookUp (message: Message, args: string[]) {
     return message.channel.send(errResult.displayText)
   }
 
-  if (data.Rank === 'ADMINISTRATOR') {
-    badges += emojiUtils.polytoria + ' '
-  }
-
-  if (data.MembershipType !== 'NONE') {
-    badges += emojiUtils.star + ' '
-  }
-
   const embed = new MessageEmbed({
-    title: data.Username + badges,
-    url: `https://polytoria.com/user/${data.ID}`,
-    description: data.Description,
+    title: data.username,
+    description: data.description,
     color: '#ff5454',
     thumbnail: {
-      url: `https://polytoria.com/assets/thumbnails/avatars/${data.AvatarHash}.png`
+      url: `https://c0.ptacdn.com/thumbnails/avatars/${data.avatarUrl}`
     },
     fields: [
       {
-        name: 'User ID',
-        value: data.ID.toString(),
-        inline: true
-      },
-      {
-        name: 'Rank',
-        value: stringUtils.capitalizeString(data.Rank),
-        inline: true
-      },
-      {
-        name: 'Membership Type',
-        value: stringUtils.capitalizeString(data.MembershipType),
-        inline: true
-      },
-      {
-        name: 'Profile Views',
-        value: data.ProfileViews.toLocaleString(),
-        inline: true
-      },
-      {
-        name: 'Item Sales',
-        value: data.ItemSales.toLocaleString(),
-        inline: true
-      },
-      {
-        name: 'Forum Posts',
-        value: data.ForumPosts.toLocaleString(),
-        inline: true
-      },
-      {
-        name: 'Trade value',
-        value: data.TradeValue.toLocaleString(),
-        inline: true
-      },
-      {
         name: 'Joined At',
-        value: dateUtils.atomTimeToDisplayTime(data.JoinedAt),
+        value: dateUtils.atomTimeToDisplayTime(data.registeredAt),
         inline: true
       },
       {
         name: 'Last seen at',
-        value: dateUtils.atomTimeToDisplayTime(data.LastSeenAt),
+        value: dateUtils.atomTimeToDisplayTime(data.lastSeenAt),
         inline: true
       }
     ]
