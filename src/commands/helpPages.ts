d82f9d0b80f99bbe50e2c399359c3f21fef17e87
StarManTheGamer <68450308+StarManTheGamer@users.noreply.github.com>
2023-05-11 18:26:22 -0500

    remove unavailable commands from help cmd

--- src/commands/helpPages.ts
+++ src/commands/helpPages.ts
@@ -6,8 +6,8 @@ export default [
       inline: false
     },
     {
-      name: 'catalog `id`',
-      value: 'View Catalog item data by Catalog item ID\n`id` - Catalog item ID',
+      name: 'info',
+      value: 'Information about Polytoria Community Bot',
       inline: false
     },
     {
@@ -16,106 +16,8 @@ export default [
       inline: false
     },
     {
-      name: 'avatar `username`',
-      value: "View user's avatar\n`username` - User's username you want to view",
-      inline: false
-    }
-  ],
-  [
-    {
-      name: 'game `id`',
-      value: 'View game info from game ID\n`id` - Game id',
-      inline: false
-    },
-    {
-      name: 'ping',
-      value: 'Checks is the bot are online.',
-      inline: false
-    },
-    {
-      name: 'status',
-      value: 'View Polytoria web status.',
-      inline: false
-    },
-    {
       name: 'cookie',
       value: '🍪',
-      inline: false
-    },
-    {
-      name: 'friends `username`',
-      value: "View targeted user's friends\n`username` - User's username you want to view",
-      inline: false
-    }
-  ],
-  [
-    {
-      name: 'guilds `id`',
-      value: "View targeted guild's information\n`id` - Guild's ID you want to view",
-      inline: false
-    },
-    {
-      name: 'info',
-      value: "Bot's Information with Invite, Version, and more.",
-      inline: false
-    },
-    {
-      name: 'card `username`',
-      value: "Gives you an image with information about specified user\n`username` - User's username you want to view",
-      inline: false
-    },
-    {
-      name: 'inventory `username`',
-      value: "Let's you see the inventory of specified user\n`username` - User's username you want to view",
-      inline: false
-    },
-    {
-      name: 'level `username`',
-      value: "Gives you information about specified user\n`username` - User's username you want to view",
-      inline: false
-    }
-  ],
-  [
-    {
-      name: '404-random-catalog',
-      value: 'Shows you deleted and previous old images from the CDN!',
-      inline: false
-    },
-    {
-      name: 'random-banner',
-      value: 'Gives you a random banner from the games page, either been removed or currently used.',
-      inline: false
-    },
-    {
-      name: 'random-catalog',
-      value: 'Gives you a random asset from the Polytoria Shop (hat, shirt, pants, etc)',
-      inline: false
-    },
-    {
-      name: 'random-game',
-      value: 'Gives you a random game from Polytoria',
-      inline: false
-    },
-    {
-      name: 'random-guild',
-      value: 'Gives you a random guild from Polytoria',
-      inline: false
-    }
-  ],
-  [
-    {
-      name: 'random-user',
-      value: 'Gives you a random user from Polytoria',
-      inline: false
-    },
-    {
-      name: 'catalog-search `name`',
-      value: 'Search for asset in the shop\n`name` - Name of the asset',
-      inline: false
-    },
-    {
-      name: 'toolbox `name`',
-      value: 'Search for a model in the library\n`name` - Name of the asset',
       inline: false
     }
   ]
