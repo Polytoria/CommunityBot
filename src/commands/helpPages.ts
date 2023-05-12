f15055bc5fecea5d100f40459fafe9c9446dfd9a
StarManTheGamer <68450308+StarManTheGamer@users.noreply.github.com>
2023-05-11 21:46:37 -0500

    Updated help command

--- src/commands/helpPages.ts
+++ src/commands/helpPages.ts
@@ -1,6 +1,16 @@
 export default [
   [
     {
+      name: 'lookup `username/id` `type(optional)`',
+      value: 'Lookup user from username or ID\n`username/id` - Their username or id\n`type` - Type, `id` or `username`',
+      inline: false
+    },
+    {
+      name: 'random-user',
+      value: 'Gives you a random user from Polytoria',
+      inline: false
+    },
+    {
       name: 'help',
       value: "Command you're using right now.",
       inline: false
@@ -10,15 +20,8 @@ export default [
       value: 'Information about Polytoria Community Bot',
       inline: false
     },
-    {
-      name: 'lookup `username/id` `type(optional)`',
-      value: 'Lookup user from username or ID\n`username/id` - Their username or id\n`type` - Type, `id` or `username`',
-      inline: false
-    },
-    {
-      name: 'cookie',
-      value: '🍪',
-      inline: false
-    }
+  ],
+  [
+
   ]
 ]
