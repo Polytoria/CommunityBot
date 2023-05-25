67b7be6957dd7519d25d5cf45bdb43511d9ce9f4
StarManTheGamer <68450308+StarManTheGamer@users.noreply.github.com>
2023-05-25 17:33:41 -0500

    restored store command

---
+++ src/commands/store.ts
@@ -0,0 +1,57 @@
+import { Message, MessageEmbed } from 'discord.js'
+import axios from 'axios'
+import { responseHandler } from '../utils/responseHandler.js'
+import { dateUtils } from '../utils/dateUtils.js'
+import emojiUtils from '../utils/emojiUtils.js'
+
+export async function store(message: Message, args: string[]) {
+  const assetID = parseInt(args[0])
+
+  const response = await axios.get(`https://api.polytoria.com/v1/store/${assetID}`, {
+    validateStatus: () => true
+  })
+  const data = response.data
+  const creator = data.creator
+
+  const errResult = responseHandler.checkError(response)
+
+  if (errResult.hasError === true) {
+    return message.channel.send(errResult.displayText)
+  }
+
+  const embed = new MessageEmbed({
+    title: data.name + ' ' + (data.isLimited === true ? emojiUtils.star : ''),
+    description: data.description === '' ? 'No description set.' : data.description,
+    url: `https://polytoria.com/store/${data.id}`,
+    thumbnail: {
+      url: `${data.thumbnail}`
+    },
+    color: '#ff5454',
+    fields: [
+      {
+        name: 'Creator',
+        value: creator.name,
+        inline: true
+      },
+      {
+        name: 'Price',
+        value: data.price.toString(),
+        inline: true
+      },
+      {
+        name: 'Sales',
+        value: data.sales.toString(),
+        inline: true
+      },
+      {
+        name: 'Created At',
+        value: dateUtils.atomTimeToDisplayTime(data.createdAt),
+        inline: true
+      }
+    ]
+  })
+
+  return message.channel.send({
+    embeds: [embed]
+  })
+}
