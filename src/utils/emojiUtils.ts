509d236d333ec8b314820353e2f7026e7751290c
StarManTheGamer <68450308+StarManTheGamer@users.noreply.github.com>
2023-07-21 15:39:15 -0500

    updated brick + poly icon, remove stud icon

--- src/utils/emojiUtils.ts
+++ src/utils/emojiUtils.ts
@@ -7,10 +7,9 @@
 export default {
   checkmark: '<:checkmark:919148026520539146>',
   warning: '<:warning:920132963461627964>',
-  stud: '<:stud:905987085347983411>',
-  brick: '<:brick:905987077995376640>',
+  brick: '<:brick:1132041322467958834>',
   loading: '<a:loading:943402536805732382>',
-  polytoria: '<:Polytoria:906009712221376554>',
+  polytoria: '<:polytoria:934263002704793600>',
   star: '<:ProMember:906016237748879392>',
   shop: '<:Shop:914106876143230976>',
   users: '<:Users:914059858276409364>',
