8a584e77fd76bcf52401caeb634124adbdfaec43
StarManTheGamer <68450308+StarManTheGamer@users.noreply.github.com>
2023-08-14 02:27:10 -0500

    clear info command (needs redone too)

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -1,16 +1,5 @@
-import { MessageEmbed, Message } from 'discord.js'
+import { EmbedBuilder, Message } from 'discord.js'
 
 export async function info (message: Message) {
-  const embed: MessageEmbed = new MessageEmbed()
 
-  embed.setThumbnail('https://starmanthegamer.com/icon.png')
-  embed.setColor('#fe5953')
-  embed.setTitle('Polytoria Community Bot')
-  embed.setURL('https://discord.com/api/oauth2/authorize?client_id=905979909049028649&permissions=414464724032&scope=bot')
-  embed.addField('Version', `Running version ${process.env.npm_package_version}`)
-  embed.addField('Invite our Bot', 'Tap the title to invite our bot directly to your server!')
-  embed.addField('We are open-sourced!', 'https://github.com/Polytoria/Polytoria-Community-Bot')
-  embed.addField('Rewritten by', 'Shiggy, DevPixels, baggy, and more!')
-  embed.setFooter('Thanks to all collaborators of the project!')
-  await message.channel.send({ embeds: [embed] })
 }
