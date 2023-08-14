45442e7e79b25a777f4558dd3ba85b54bbc9bf6f
StarManTheGamer <68450308+StarManTheGamer@users.noreply.github.com>
2023-08-14 14:14:51 -0500

    return info command

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -1,5 +1,18 @@
 import { EmbedBuilder, Message } from 'discord.js'
 
 export async function info (message: Message) {
+  const embed: EmbedBuilder = new EmbedBuilder()
+    .setColor(0xFF5454)
+    .setTitle('Polytoria Community Bot')
+    .setURL('https://discord.com/api/oauth2/authorize?client_id=905979909049028649&permissions=414464724032&scope=bot')
+    .setThumbnail('https://starmanthegamer.com/icon.png')
+    .addFields(
+      { name: 'Version', value: 'Currently running version: ' },
+      { name: 'Invite our Bot', value: 'Tap the title to invite our bot directly to your server!', inline: true },
+      { name: 'We are open-sourced!', value: 'https://github.com/Polytoria/Polytoria-Community-Bot', inline: true },
+      { name: 'Rewritten by', value: 'Shiggy, DevPixels, baggy, and more!', inline: true }
+    )
+    .setFooter({ text: 'Thank you for using Polytoria Community Bot!', iconURL: 'https://starmanthegamer.com/icon.png' });
 
+  await message.channel.send({ embeds: [embed] })
 }
