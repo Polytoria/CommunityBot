3430d42baaf885d6bcb2d8d4d4bf20303a75579d
StarManTheGamer <68450308+StarManTheGamer@users.noreply.github.com>
2023-08-19 16:27:32 -0500

    add back guild members

--- src/commands/guild.ts
+++ src/commands/guild.ts
@@ -1,4 +1,4 @@
-import { Message, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } from 'discord.js'
+import { Message, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType, BaseInteraction } from 'discord.js'
 import axios from 'axios'
 import { responseHandler } from '../utils/responseHandler.js'
 import { dateUtils } from '../utils/dateUtils.js'
@@ -59,16 +59,163 @@ export async function guild (message: Message, args: string[]): Promise<Message 
     embed.setImage(data.banner)
   }
 
-  const actionRow = new ActionRowBuilder<ButtonBuilder>()
-  .addComponents(
-    new ButtonBuilder()
-      .setURL(`https://polytoria.com/guilds/${data.id}`)
-      .setLabel('View on Polytoria')
-      .setStyle(ButtonStyle.Link)
-  )
+  const memberResponse = await axios.get(`https://api.polytoria.com/v1/guilds/${guildID}/members?page=1&limit=15`)
+  const memberData = memberResponse.data.members
+  const memberUsernames = memberData
+    .map((member: any) => `[${member.user.username}](https://polytoria.com/users/${member.user.id})`)
+    .join('\n')
 
-  return message.reply({
+  const memberEmbed = new EmbedBuilder()
+    .setTitle(data.name + ' - Members ' + (data.isVerified === true ? emojiUtils.checkmark : ''))
+    .setDescription(memberUsernames)
+    .setThumbnail(data.thumbnail)
+    .setColor(data.color)
+    .setURL('https://polytoria.com/guilds/' + data.id.toString())
+
+  const guildButton = new ButtonBuilder()
+    .setLabel('Guild')
+    .setStyle(ButtonStyle.Primary)
+    .setCustomId('guild_button')
+
+  const membersButton = new ButtonBuilder()
+    .setLabel('Members')
+    .setStyle(ButtonStyle.Primary)
+    .setCustomId('members_button')
+
+  const nextButton = new ButtonBuilder()
+    .setLabel('Next')
+    .setStyle(ButtonStyle.Success)
+    .setCustomId('next_button')
+
+  const prevButton = new ButtonBuilder()
+    .setLabel('Previous')
+    .setStyle(ButtonStyle.Danger)
+    .setCustomId('prev_button')
+
+    const actionRow = new ActionRowBuilder<ButtonBuilder>()
+    .addComponents(
+      new ButtonBuilder()
+        .setURL(`https://polytoria.com/guilds/${data.id}`)
+        .setLabel('View on Polytoria')
+        .setStyle(ButtonStyle.Link)
+    )
+    .addComponents(
+      membersButton
+    )
+
+  const reply = await message.reply({
     embeds: [embed],
     components: [actionRow]
   })
+
+  let memberPage = 1
+
+  const collector = reply.createMessageComponentCollector({
+    componentType: ComponentType.Button,
+    filter: (interaction: BaseInteraction) => (
+      interaction.isButton() && interaction.user.id === message.author.id
+    ),
+    time: 60000
+  })
+
+  collector.on('collect', async (interaction) => {
+    await interaction.deferUpdate() // Defer the interaction
+
+    if (interaction.customId === 'members_button') {
+      membersButton.setDisabled(true)
+      nextButton.setDisabled(false)
+      prevButton.setDisabled(true)
+
+      await reply.edit({
+        embeds: [memberEmbed],
+        components: [
+          new ActionRowBuilder<ButtonBuilder>().addComponents(guildButton, prevButton, nextButton)
+        ]
+      })
+
+      const guildButtonCollector = reply.createMessageComponentCollector({
+        componentType: ComponentType.Button,
+        filter: (btnInteraction: BaseInteraction) => (
+          btnInteraction.isButton() &&
+          btnInteraction.customId === 'guild_button' &&
+          btnInteraction.user.id === message.author.id
+        ),
+        time: 60000
+      })
+
+      guildButtonCollector.on('collect', async () => {
+        membersButton.setDisabled(false)
+        nextButton.setDisabled(true)
+        prevButton.setDisabled(true)
+        await reply.edit({
+          embeds: [embed],
+          components: [actionRow]
+        })
+      })
+
+      const nextButtonCollector = reply.createMessageComponentCollector({
+        componentType: ComponentType.Button,
+        filter: (btnInteraction: BaseInteraction) => (
+          btnInteraction.isButton() &&
+          btnInteraction.customId === 'next_button' &&
+          btnInteraction.user.id === message.author.id
+        ),
+        time: 60000
+      })
+
+      nextButtonCollector.on('collect', async () => {
+        const newPage = memberPage + 1
+        const newMemberResponse = await axios.get(`https://api.polytoria.com/v1/guilds/${guildID}/members?page=${newPage}&limit=15`)
+        const newMemberData = newMemberResponse.data.members
+        const newMemberUsernames: string = newMemberData.map((member: any) => member.user.username).join('\n')
+
+        memberPage = newPage
+        memberEmbed.setDescription(newMemberUsernames)
+
+        nextButton.setDisabled(false)
+        prevButton.setDisabled(false)
+
+        await reply.edit({
+          embeds: [memberEmbed],
+          components: [
+            new ActionRowBuilder<ButtonBuilder>().addComponents(guildButton, prevButton, nextButton)
+          ]
+        })
+      })
+
+      const prevButtonCollector = reply.createMessageComponentCollector({
+        componentType: ComponentType.Button,
+        filter: (btnInteraction: BaseInteraction) => (
+          btnInteraction.isButton() &&
+          btnInteraction.customId === 'prev_button' &&
+          btnInteraction.user.id === message.author.id
+        ),
+        time: 60000
+      })
+
+      prevButtonCollector.on('collect', async () => {
+        if (memberPage > 1) {
+          const newPage = memberPage - 1
+          const newMemberResponse = await axios.get(`https://api.polytoria.com/v1/guilds/${guildID}/members?page=${newPage}&limit=15`)
+          const newMemberData = newMemberResponse.data.members
+          const newMemberUsernames: string = newMemberData.map((member: any) => member.user.username).join('\n')
+
+          memberPage = newPage
+          memberEmbed.setDescription(newMemberUsernames)
+
+          nextButton.setDisabled(false)
+          prevButton.setDisabled(false)
+
+          await reply.edit({
+            embeds: [memberEmbed],
+            components: [
+              new ActionRowBuilder<ButtonBuilder>().addComponents(guildButton, prevButton, nextButton)
+            ]
+          })
+        }
+      })
+    }
+  })
+
+  return reply
 }
