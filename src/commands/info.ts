3bc92c73b39a4e586bda92de4191cc50f8e09636
StarManTheGamer <68450308+StarManTheGamer@users.noreply.github.com>
2023-08-19 15:48:38 -0500

    Buttons to info cmd

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -1,6 +1,19 @@
-import { EmbedBuilder, Message } from 'discord.js'
+import { EmbedBuilder, Message, ActionRowBuilder, ButtonBuilder, ButtonStyle } from 'discord.js'
 
 export async function info (message: Message) {
+  const invite = new ButtonBuilder()
+    .setLabel('Invite us!')
+    .setURL('https://discord.com/api/oauth2/authorize?client_id=905979909049028649&permissions=414464724032&scope=bot')
+    .setStyle(ButtonStyle.Link)
+
+  const github = new ButtonBuilder()
+    .setLabel('GitHub')
+    .setURL('https://github.com/Polytoria/CommunityBot')
+    .setStyle(ButtonStyle.Link)
+
+  const row = new ActionRowBuilder<ButtonBuilder>()
+    .addComponents(github, invite)
+
   const embed: EmbedBuilder = new EmbedBuilder()
     .setColor(0xFF5454)
     .setTitle('Polytoria Community Bot')
@@ -8,11 +21,9 @@ export async function info (message: Message) {
     .setThumbnail('https://starmanthegamer.com/icon.png')
     .addFields(
       { name: 'Version', value: `Currently running version: ${process.env.npm_package_version}` },
-      { name: 'Invite our Bot', value: 'Tap the title to invite our bot directly to your server!', inline: true },
-      { name: 'We are open-sourced!', value: 'https://github.com/Polytoria/Polytoria-Community-Bot', inline: true },
-      { name: 'Rewritten by', value: 'Shiggy, DevPixels, baggy, and more!', inline: true }
+      { name: 'Contributed by:', value: 'baggy, Hu Tao, Shiggy, and many more!', inline: true }
     )
     .setFooter({ text: 'Thank you for using Polytoria Community Bot!', iconURL: 'https://starmanthegamer.com/icon.png' })
 
-  await message.channel.send({ embeds: [embed] })
+  await message.channel.send({ embeds: [embed], components: [row] })
 }
