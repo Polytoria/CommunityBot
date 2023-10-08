7413c85c9a61da4661c7e5c6902518edc7ece297
StarManTheGamer <68450308+StarManTheGamer@users.noreply.github.com>
2023-10-08 14:54:19 -0500

    remove: type after message
    reason: re-randomize button was broken when using this method, and i couldn't figure out how.
    
    other reason: we will be removing message commands in the future for slash commands, so there's no reason to have this right now.

--- src/commands/random.ts
+++ src/commands/random.ts
@@ -8,115 +8,100 @@ import { randomStore } from './random/randomStore.js'
 
 export async function random (message: Message, args: any[]) {
   let InitialType: any = null
-  if (args.length === 0) {
-    const embed = new EmbedBuilder()
-      .setTitle('Randomizer!')
-      .setColor(0xFF5454)
-      .setDescription('Pick an asset type to get a random one! (or specify "place", "user", "guild", or "store" when executing the command)')
 
-    const placeOption = new StringSelectMenuOptionBuilder()
-      .setLabel('🎮 Places')
-      .setDescription('Get a random place that is available on Polytoria!')
-      .setValue('place')
+  const embed = new EmbedBuilder()
+    .setTitle('Randomizer!')
+    .setColor(0xFF5454)
+    .setDescription("Welcome to the randomizer! Pick a feature on Polytoria that you would like us to randomize for you.")
 
-    const userOption = new StringSelectMenuOptionBuilder()
-      .setLabel('🗣️ Users')
-      .setDescription('Get a random user that is on Polytoria!')
-      .setValue('user')
+  const placeOption = new StringSelectMenuOptionBuilder()
+    .setLabel('🎮 Places')
+    .setDescription('Get a random place that is available on Polytoria!')
+    .setValue('place')
 
-    const guildOption = new StringSelectMenuOptionBuilder()
-      .setLabel('🫂 Guilds')
-      .setDescription('Get a random guild that is available on Polytoria!')
-      .setValue('guild')
+  const userOption = new StringSelectMenuOptionBuilder()
+    .setLabel('🗣️ Users')
+    .setDescription('Get a random user that is on Polytoria!')
+    .setValue('user')
 
-    const storeOption = new StringSelectMenuOptionBuilder()
-      .setLabel('🏪 Store')
-      .setDescription('Get a random asset that is available on Polytoria!')
-      .setValue('store')
+  const guildOption = new StringSelectMenuOptionBuilder()
+    .setLabel('🫂 Guilds')
+    .setDescription('Get a random guild that is available on Polytoria!')
+    .setValue('guild')
 
-    const selectMenu = new StringSelectMenuBuilder()
-      .setPlaceholder('Asset Type...')
-      .setCustomId('select')
-      .addOptions(
-        placeOption,
-        userOption,
-        guildOption,
-        storeOption
-      )
+  const storeOption = new StringSelectMenuOptionBuilder()
+    .setLabel('🏪 Store')
+    .setDescription('Get a random asset that is available on Polytoria!')
+    .setValue('store')
 
-    const actionRow = new ActionRowBuilder<StringSelectMenuBuilder>()
-      .addComponents(
-        selectMenu
-      )
+  const selectMenu = new StringSelectMenuBuilder()
+    .setPlaceholder('Asset Type...')
+    .setCustomId('select')
+    .addOptions(
+      placeOption,
+      userOption,
+      guildOption,
+      storeOption
+    )
 
-    const reply = await message.reply({
-      embeds: [embed],
-      components: [actionRow]
-    })
+  const actionRow = new ActionRowBuilder<StringSelectMenuBuilder>()
+    .addComponents(
+      selectMenu
+    )
 
-    const selectCollector = reply.createMessageComponentCollector({
-      componentType: ComponentType.StringSelect,
-      filter: (interaction: BaseInteraction) => (
-        interaction.isStringSelectMenu() && interaction.user.id === message.author.id
-      ),
-      time: 60000
-    })
+  const reply = await message.reply({
+    embeds: [embed],
+    components: [actionRow]
+  })
 
-    selectCollector.on('collect', async (interaction) => {
-      if (interaction.customId === 'select') {
-        update(interaction.values[0], reply)
-      }
-    })
+  const selectCollector = reply.createMessageComponentCollector({
+    componentType: ComponentType.StringSelect,
+    filter: (interaction: BaseInteraction) => (
+      interaction.isStringSelectMenu() && interaction.user.id === message.author.id
+    ),
+    time: 60000
+  })
 
-    const buttonCollector = reply.createMessageComponentCollector({
-      componentType: ComponentType.Button,
-      filter: (interaction: BaseInteraction) => (
-        interaction.isButton() && interaction.user.id === message.author.id
-      ),
-      time: 60000
-    })
-
-    buttonCollector.on('collect', async (interaction) => {
-      if (interaction.customId === 'redo_button') {
-        update(InitialType, reply)
-      }
-    })
-  } else if (args[0]) {
-    if (args[0] === 'place' || args[0] === 'user' || args[0] === 'guild' || args[0] === 'store') {
-      update(args[0], null)
-    } else {
-      message.reply('That asset type doesn\'t exist! Please try again...')
+  selectCollector.on('collect', async (interaction) => {
+    if (interaction.customId === 'select') {
+      InitialType = interaction.values[0] // Update InitialType here
+      update(InitialType, reply)
     }
-  }
+  })
+
+  const buttonCollector = reply.createMessageComponentCollector({
+    componentType: ComponentType.Button,
+    filter: (interaction: BaseInteraction) => (
+      interaction.isButton() && interaction.user.id === message.author.id && interaction.customId === 'redo_button'
+    ),
+    time: 60000
+  })
+
+  buttonCollector.on('collect', async (interaction) => {
+    if (InitialType !== null) {
+      update(InitialType, reply)
+    }
+  })
 
   async function update (id: string, reply: any) {
-    if (InitialType === null) { InitialType = id }
     let Response: any = null
     switch (id) {
       case 'place':
         Response = await randomPlace(message, args)
-        InitialType = 'place'
         break
       case 'user':
         Response = await randomUser(message, args)
-        InitialType = 'user'
         break
       case 'guild':
         Response = await randomGuild(message, args)
-        InitialType = 'guild'
         break
       case 'store':
         Response = await randomStore(message, args)
-        InitialType = 'store'
         break
     }
 
     if (Response !== null) {
-      if (reply !== null) {
-        await reply.edit(Response)
-      } else {
-        message.reply(Response)
-      }
+      await reply.edit(Response)
     }
   }
 }
