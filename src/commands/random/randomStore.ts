2a397fb5723d39e4f288c58223312433393bff9f
StarManTheGamer <68450308+StarManTheGamer@users.noreply.github.com>
2023-12-20 22:41:05 -0600

    add tags random store api

--- src/commands/random/randomStore.ts
+++ src/commands/random/randomStore.ts
@@ -60,6 +60,11 @@ export async function randomStore (message: Message, args: string[]) {
 
   const assetType = data.type.toLowerCase()
   if (!['audio', 'decal', 'mesh'].includes(assetType)) {
+    if (data.tags && data.tags.length > 0 && data.tags[0] !== '') {
+      embed.setDescription(data.description === '' ? 'No description set.' : data.description + '\n\n**Tags:** ' + (data.tags as string[]).map(tag => `\`${tag}\``).join(', '))
+    } else {
+      embed.setDescription(data.description === '' ? 'No description set.' : data.description)
+    }
     embed.addFields(
       {
         name: 'Price',
