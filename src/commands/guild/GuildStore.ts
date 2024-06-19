ce32d81f10cc9a408f394a9575fe7cea23bc24c1
StarManTheGamer <68450308+StarManTheGamer@users.noreply.github.com>
2024-06-19 18:03:53 -0500

    add total assets/page to guild store embed

--- src/commands/guild/GuildStore.ts
+++ src/commands/guild/GuildStore.ts
@@ -1,16 +1,22 @@
 import axios from 'axios'
 
 export async function fetchStore (id: number, page: number) {
+  // Fetch the store data from the API
   const storeResponse = await axios.get(`https://api.polytoria.com/v1/guilds/${id}/store?page=${page}&limit=15`)
   const storeData = storeResponse.data.assets
+  const totalAssets = storeResponse.data.total
+  const totalPages = storeResponse.data.pages
 
+  // If no store assets are found, return a message
   if (storeData.length === 0) {
     return 'There are no store assets in this guild.'
   }
 
+  // Prepare the store assets list
   const storeItems = storeData
-    .map((item: any, index: number) => '``' + (index + 1) + '``' + ` [${item.name}](https://polytoria.com/store/${item.id})`)
+    .map((item: any, index: number) => `\`\`${(index + 1) + (page - 1) * 15}\`\` [${item.name}](https://polytoria.com/store/${item.id})`)
     .join('\n')
 
-  return storeItems
+  // Format and return the result string
+  return `> **Total Assets: ${totalAssets}\n > Page ${page}/${totalPages}**\n\n${storeItems}`
 }
