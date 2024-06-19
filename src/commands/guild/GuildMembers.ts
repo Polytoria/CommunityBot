b9f84820fb538b89f92d4f72fa6241ebe66e139d
StarManTheGamer <68450308+StarManTheGamer@users.noreply.github.com>
2024-06-19 18:31:51 -0500

    remove error crap

--- src/commands/guild/GuildMembers.ts
+++ src/commands/guild/GuildMembers.ts
@@ -1,23 +1,22 @@
 import axios from 'axios'
 
 export async function fetchMembers (id: number, page: number) {
-  try {
-    const memberResponse = await axios.get(`https://api.polytoria.com/v1/guilds/${id}/members?page=${page}&limit=15`)
-    const memberData = memberResponse.data.members
-    const totalMembers = memberResponse.data.total
-    const totalPages = memberResponse.data.pages
+  // Fetch the members data from the API
+  const memberResponse = await axios.get(`https://api.polytoria.com/v1/guilds/${id}/members?page=${page}&limit=15`)
+  const memberData = memberResponse.data.members
+  const totalMembers = memberResponse.data.total
+  const totalPages = memberResponse.data.pages
 
-    const memberUsernames = memberData
-      .map((member: any) => `[${member.user.username}](https://polytoria.com/users/${member.user.id})`)
-      .join('\n')
-
-    if (memberData.length === 0) {
-      return 'There are no members in this guild.'
-    }
-
-    return `> **Total Members: ${totalMembers}\n > Page ${page}/${totalPages}**\n\n${memberUsernames}`
-  } catch (error) {
-    console.error('Error fetching members:', error)
-    return 'An error occurred while fetching the members.'
+  // If no members are found, return a message
+  if (memberData.length === 0) {
+    return 'There are no members in this guild.'
   }
+
+  // Prepare the members' usernames list
+  const memberUsernames = memberData
+    .map((member: any) => `[${member.user.username}](https://polytoria.com/users/${member.user.id})`)
+    .join('\n')
+
+  // Format and return the result string
+  return `> **Total Members: ${totalMembers}\n > Page ${page}/${totalPages}**\n\n${memberUsernames}`
 }
