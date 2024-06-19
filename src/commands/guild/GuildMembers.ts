8d7ae8a106bdea04214e8714281dd1516b259d95
StarManTheGamer <68450308+StarManTheGamer@users.noreply.github.com>
2024-06-19 17:43:12 -0500

    Added page count to guild members embed

--- src/commands/guild/GuildMembers.ts
+++ src/commands/guild/GuildMembers.ts
@@ -1,15 +1,23 @@
 import axios from 'axios'
 
 export async function fetchMembers (id: number, page: number) {
-  const memberResponse = await axios.get(`https://api.polytoria.com/v1/guilds/${id}/members?page=${page}&limit=15`)
-  const memberData = memberResponse.data.members
-  const memberUsernames = memberData
-    .map((member: any) => `[${member.user.username}](https://polytoria.com/users/${member.user.id})`)
-    .join('\n')
+  try {
+    const memberResponse = await axios.get(`https://api.polytoria.com/v1/guilds/${id}/members?page=${page}&limit=15`)
+    const memberData = memberResponse.data.members
+    const totalMembers = memberResponse.data.total
+    const totalPages = memberResponse.data.pages
 
-  if (memberData.length === 0) {
-    return 'There are no members in this guild.'
+    const memberUsernames = memberData
+      .map((member: any) => `[${member.user.username}](https://polytoria.com/users/${member.user.id})`)
+      .join('\n')
+
+    if (memberData.length === 0) {
+      return 'There are no members in this guild.'
+    }
+
+    return `> **Total Members: ${totalMembers} - Page ${page}/${totalPages}**\n\n${memberUsernames}`
+  } catch (error) {
+    console.error('Error fetching members:', error)
+    return 'An error occurred while fetching the members.'
   }
-
-  return memberUsernames
 }
