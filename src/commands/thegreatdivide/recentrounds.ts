f6ddb8f983c08592c101f4f4f386c81db2b8e478
StarManTheGamer <68450308+StarManTheGamer@users.noreply.github.com>
2024-07-14 23:38:27 -0500

    convert api for recent rounds

--- src/commands/thegreatdivide/recentrounds.ts
+++ src/commands/thegreatdivide/recentrounds.ts
@@ -3,23 +3,23 @@ import axios from 'axios'
 
 export async function handleRecentRounds (interaction: CommandInteraction) {
   try {
-    const roundsResponse = await axios.get('https://api.polytoria.com/v1/rounds/')
-    const roundsData = roundsResponse.data.rounds
+    const roundsResponse = await axios.get('https://stats.silly.mom/rounds')
+    const roundsData = roundsResponse.data.results
 
     const embed = new EmbedBuilder()
       .setTitle('Recent Rounds')
       .setURL('https://polytoria.com/event/the-great-divide')
       .setThumbnail('https://c0.ptacdn.com/static/assets/events/great-divide-assets/logo.d7df4fce.png')
-      .setFooter({ text: 'Not already enrolled in a team? Join the phantoms!', iconURL: 'https://c0.ptacdn.com/guilds/icons/bbLypENoqMEipAPsPK5h-kLSaysV6VGB.png' })
+      .setFooter({ text: 'The Great Divide has concluded as of July 14, 2024.', iconURL: 'https://c0.ptacdn.com/guilds/icons/zosu3Vgf_MzLmCkJvmgDiUgeSy74AGBy.png' })
       .setColor(0x0099FF)
 
     // Ensuring roundsData is an array
     if (Array.isArray(roundsData)) {
-      roundsData.forEach((round: { id: number; place: string; winningTeam: string; duration: number; map: string | null }) => {
+      roundsData.forEach((round: { id: number; place: string; winner: string; duration: number; map: string | null}) => {
         const mapInfo = round.map ? `- Map: ${round.map}` : ''
         embed.addFields({
           name: `Round ${round.id} - ${round.place} ${mapInfo}`,
-          value: `Winning Team: ${round.winningTeam} - Duration: ${round.duration} seconds`,
+          value: `Winning Team: ${round.winner} - Duration: ${round.duration} seconds`,
           inline: false
         })
       })
