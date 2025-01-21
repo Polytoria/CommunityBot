833a6cd981042157c012fe44f043339e5ad41bf3
StarManTheGamer <68450308+StarManTheGamer@users.noreply.github.com>
2025-01-21 01:11:31 -0600

    Migrate search cmds to fetch

--- src/commands/search/place-search.ts
+++ src/commands/search/place-search.ts
@@ -1,8 +1,7 @@
 import { EmbedBuilder, CommandInteraction } from 'discord.js'
-import axios from 'axios'
 import { responseHandler } from '../../utils/responseHandler.js'
 
-export async function placeSearch (interaction:CommandInteraction) {
+export async function placeSearch (interaction: CommandInteraction) {
   let searchQuery = ''
   // @ts-expect-error
   const queryInput = interaction.options.getString('query')
@@ -13,17 +12,19 @@ export async function placeSearch (interaction:CommandInteraction) {
 
   await interaction.deferReply()
 
-  const response = await axios.get(
-    `https://polytoria.com/api/places?page=1&search=${searchQuery}&genre=all&sort=popular`,
-    { params: {}, validateStatus: () => true }
+  // Fetch data using fetch
+  const response = await fetch(
+    `https://polytoria.com/api/places?page=1&search=${encodeURIComponent(searchQuery)}&genre=all&sort=popular`
   )
-  const data = response.data.data
+  const errResult = await responseHandler.checkError(response)
 
-  const errResult = responseHandler.checkError(response)
-
-  if (errResult.hasError === true) {
-    return await interaction.editReply(errResult.displayText)
+  // Handle errors
+  if (errResult.hasError && errResult.embed) {
+    return await interaction.editReply({ embeds: [errResult.embed] })
   }
+
+  const result = await response.json()
+  const data = result.data
 
   const embed = new EmbedBuilder({
     title: `Search results for "${searchQuery}"`,
@@ -35,13 +36,11 @@ export async function placeSearch (interaction:CommandInteraction) {
 
   const topPlaces = data.slice(0, 15)
   for (const place of topPlaces) {
-    description += `\`${index}\` [${place.name}](https://polytoria.com/places/${place.id}) ${
-      ''
-  }\n`
+    description += `\`${index}\` [${place.name}](https://polytoria.com/places/${place.id})\n`
     index++
   }
 
-  embed.setDescription(description)
+  embed.setDescription(description || 'No results found.')
 
   return await interaction.editReply({
     embeds: [embed]
--- src/commands/search/store-search.ts
+++ src/commands/search/store-search.ts
@@ -1,9 +1,8 @@
 import { EmbedBuilder, CommandInteraction } from 'discord.js'
-import axios from 'axios'
 import { responseHandler } from '../../utils/responseHandler.js'
 import emojiUtils from '../../utils/emojiUtils.js'
 
-export async function storeSearch (interaction:CommandInteraction) {
+export async function storeSearch (interaction: CommandInteraction) {
   let searchQuery = ''
   // @ts-expect-error
   const queryInput = interaction.options.getString('query')
@@ -14,17 +13,17 @@ export async function storeSearch (interaction:CommandInteraction) {
 
   await interaction.deferReply()
 
-  const response = await axios.get(
-    `https://polytoria.com/api/store/items?types[]=hat&types[]=tool&types[]=face&types[]=shirt&types[]=pants&page=1&search=${searchQuery}&sort=createdAt&order=desc&showOffsale=false&collectiblesOnly=false`,
-    { params: {}, validateStatus: () => true }
+  const response = await fetch(
+    `https://polytoria.com/api/store/items?types[]=hat&types[]=tool&types[]=face&types[]=shirt&types[]=pants&page=1&search=${encodeURIComponent(searchQuery)}&sort=createdAt&order=desc&showOffsale=false&collectiblesOnly=false`
   )
-  const data = response.data.data
+  const errResult = await responseHandler.checkError(response)
 
-  const errResult = responseHandler.checkError(response)
-
-  if (errResult.hasError === true) {
-    return await interaction.editReply(errResult.displayText)
+  if (errResult.hasError && errResult.embed) {
+    return await interaction.editReply({ embeds: [errResult.embed] })
   }
+
+  const result = await response.json()
+  const data = result.data
 
   const embed = new EmbedBuilder({
     title: `Search results for "${searchQuery}"`,
@@ -36,12 +35,12 @@ export async function storeSearch (interaction:CommandInteraction) {
 
   for (const item of data) {
     description += `\`${index}\` [${item.name}](https://polytoria.com/store/${item.id}) ${
-    item.isLimited === true ? emojiUtils.star : ''
-  }\n`
+      item.isLimited === true ? emojiUtils.star : ''
+    }\n`
     index++
   }
 
-  embed.setDescription(description)
+  embed.setDescription(description || 'No results found.')
 
   return await interaction.editReply({
     embeds: [embed]
